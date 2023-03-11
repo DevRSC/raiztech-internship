c1e3ae4bec5f512ddd4bd7488c7afa584fbad0c3
RamilSC <ramil.cobillajr@tup.edu.ph>
2023-03-11 12:24:51 +0800

    Updated formats

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -8,35 +8,35 @@ interface Props {
 
 export default function Pagination({ currentPage, maxPage, paginate }: Props) {
   return (
-    <div className="mt-6 sm:flex sm:items-center sm:justify-between">
-      <div className="p-1 text-sm text-gray-400 ">
+    <div className='mt-6 sm:flex sm:items-center sm:justify-between'>
+      <div className='p-1 text-sm text-gray-400 '>
         Page{" "}
-        <span className="font-medium text-gray-700 dark:text-gray-100 ">
+        <span className='font-medium text-gray-700 dark:text-gray-100 '>
           {currentPage} of {maxPage}
         </span>
       </div>
 
-      <div className="mt-4 flex items-center gap-x-4 sm:mt-0">
+      <div className='mt-4 flex items-center gap-x-4 sm:mt-0'>
         <button
           onClick={() => {
             if (currentPage === 1) return;
             paginate(currentPage - 1);
           }}
-          type="button"
-          className="flex w-1/2 items-center justify-center gap-x-2 rounded-md border bg-white px-5 py-2 text-sm capitalize text-gray-700 transition-colors duration-200 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 dark:hover:bg-gray-800 sm:w-auto"
+          type='button'
+          className='flex w-1/2 items-center justify-center gap-x-2 rounded-md border bg-white px-5 py-2 text-sm capitalize text-gray-700 transition-colors duration-200 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 dark:hover:bg-gray-800 sm:w-auto'
         >
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-5 w-5 rtl:-scale-x-100"
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
+            className='h-5 w-5 rtl:-scale-x-100'
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18'
             />
           </svg>
 
@@ -44,27 +44,27 @@ export default function Pagination({ currentPage, maxPage, paginate }: Props) {
         </button>
 
         <button
-          type="button"
+          type='button'
           onClick={() => {
             if (currentPage === maxPage) return;
             paginate(currentPage + 1);
           }}
-          className="flex w-1/2 items-center justify-center gap-x-2 rounded-md border bg-white px-5 py-2 text-sm capitalize text-gray-700 transition-colors duration-200 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 dark:hover:bg-gray-800 sm:w-auto"
+          className='flex w-1/2 items-center justify-center gap-x-2 rounded-md border bg-white px-5 py-2 text-sm capitalize text-gray-700 transition-colors duration-200 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 dark:hover:bg-gray-800 sm:w-auto'
         >
           <span>Next</span>
 
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-5 w-5 rtl:-scale-x-100"
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
+            className='h-5 w-5 rtl:-scale-x-100'
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3'
             />
           </svg>
         </button>
--- components/SearchFilter.tsx
+++ components/SearchFilter.tsx
@@ -18,20 +18,20 @@ export default function SearchFilter({
 }: Props) {
   return (
     <>
-      <div className="mt-6 md:flex md:items-center md:justify-between">
-        <div className=" divide-x rounded-lg border bg-white rtl:flex-row-reverse dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-900">
+      <div className='mt-6 md:flex md:items-center md:justify-between'>
+        <div className=' divide-x rounded-lg border bg-white rtl:flex-row-reverse dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-900'>
           <button
             onClick={() => {
               viewAll();
             }}
-            className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-sm"
+            className='px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-sm'
           >
             View all
           </button>
 
           <button
             onClick={filterButton}
-            className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-sm"
+            className='px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-sm'
           >
             Filter by smaller area
           </button>
@@ -39,30 +39,30 @@ export default function SearchFilter({
           <Dropdown getFilteredRegion={getFilteredRegion} />
         </div>
 
-        <div className="relative mt-4 flex items-center md:mt-0">
-          <span className="absolute">
+        <div className='relative mt-4 flex items-center md:mt-0'>
+          <span className='absolute'>
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="mx-3 h-5 w-5 text-gray-400 dark:text-gray-600"
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth='1.5'
+              stroke='currentColor'
+              className='mx-3 h-5 w-5 text-gray-400 dark:text-gray-600'
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
               />
             </svg>
           </span>
 
           <input
-            type="text"
-            placeholder="Search"
+            type='text'
+            placeholder='Search'
             value={search}
             onChange={filterData}
-            className="block w-full rounded-lg border border-gray-200 bg-white py-1.5 pr-5 pl-11 text-gray-700 placeholder-gray-400/70 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 rtl:pr-11 rtl:pl-5 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300 md:w-80"
+            className='block w-full rounded-lg border border-gray-200 bg-white py-1.5 pr-5 pl-11 text-gray-700 placeholder-gray-400/70 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 rtl:pr-11 rtl:pl-5 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300 md:w-80'
           />
         </div>
       </div>
