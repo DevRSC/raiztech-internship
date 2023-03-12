e4b6220cb207dee17a1df3d4df3cb2062b868924
RamilSC <ramil.cobillajr@tup.edu.ph>
2023-03-12 17:49:29 +0800

    Update Dropdown.tsx

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -5,50 +5,41 @@ import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 interface Props {
   getFilteredRegion: (region: string) => void;
+  regions: string[];
 }
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-const regions = [
-  "Asia",
-  "Europe",
-  "Africa",
-  "Oceania",
-  "Americas",
-  "Polar",
-  "Antarctic Ocean",
-];
-
-export default function Dropdown({ getFilteredRegion }: Props) {
+export default function Dropdown({ getFilteredRegion, regions }: Props) {
   const filterButton = (region: string) => {
     getFilteredRegion(region);
   };
 
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as='div' className='relative inline-block text-left'>
       <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md px-5  text-gray-600 transition-colors duration-200 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-sm">
+        <Menu.Button className='inline-flex w-full justify-center gap-x-1.5 rounded-md px-5  text-gray-600 transition-colors duration-200 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-sm'>
           Regions
           <ChevronDownIcon
-            className="-mr-1 h-5 w-5 text-gray-400"
-            aria-hidden="true"
+            className='-mr-1 h-5 w-5 text-gray-400'
+            aria-hidden='true'
           />
         </Menu.Button>
       </div>
 
       <Transition
         as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
+        enter='transition ease-out duration-100'
+        enterFrom='transform opacity-0 scale-95'
+        enterTo='transform opacity-100 scale-100'
+        leave='transition ease-in duration-75'
+        leaveFrom='transform opacity-100 scale-100'
+        leaveTo='transform opacity-0 scale-95'
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
+        <Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+          <div className='py-1'>
             {regions.map((region) => (
               <Menu.Item key={region}>
                 {({ active }) => (
